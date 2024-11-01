8677f66b37a431ff0709544c369ce718b0b7d84e
William Fernandes <wiilfern1910@outlook.com>
2024-10-31 21:14:27 -0300

    Setup

---
+++ Orion Books/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Orion_Books.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
---
+++ Orion Books/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Orion_Books.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
