8e94494e14306eeab86a5ea8b954c37d7f95907d
William Fernandes <wiilfern1910@outlook.com>
2024-11-15 21:29:21 -0300

    Minor Fixes

--- Orion Books/obj/Debug/net8.0/Orion Books.AssemblyInfo.cs
+++ Orion Books/obj/Debug/net8.0/Orion Books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orion Books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0b6bc9b6db3a1dea0a9080f466a21bb7cee730")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70e72eee63866e9932cada8778001e4a7b33527")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orion Books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orion Books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
